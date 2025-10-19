6d22cf85bc61e3ea2a6e08b99c442e4e9f705fe9
Raushan kumar <raushankumar8757825@gmail.com>
2025-10-19 15:02:48 +0530

    implemeting first version of chunk iterator

--- ChunkedIterator/src/main.rs
+++ ChunkedIterator/src/main.rs
@@ -1,3 +1,29 @@
+struct gy{
+    chunksize:u8,
+    
+}
+
+
+
+fn chunk(story:&str){
+    let mut b=String::from("");
+       for ty in story.chars(){
+       if !ty.is_ascii_whitespace(){
+      if b.len()<=4{
+        b.push(ty);
+       }else {
+           println!("{}",b);
+            b.clear();
+             b.push(ty);
+       }
+    }
+      }
+      
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let  story="hello world nbsaidbf njbaigfbir jiubasdiubfgu uahsfgiberiq buuahsfu bibaeif ";
+    chunk(story);
+   
 }
